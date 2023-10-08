94e72f4d9dd7b23afbdd73d292c3873891d60962
Daniyal Idrees <>
2023-10-08 21:09:07 +0200

    unit tests added for loading details in the viewmodel

--- ui/src/test/kotlin/com/example/ui/search/viewmodel/SearchViewModelTest.kt
+++ ui/src/test/kotlin/com/example/ui/search/viewmodel/SearchViewModelTest.kt
@@ -1,10 +1,14 @@
 package com.example.ui.search.viewmodel
 
 import com.example.domain.model.UserDataResult
+import com.example.domain.usecase.GetUserDetailsUseCase
 import com.example.domain.usecase.GetUsersUseCase
-import com.example.ui.search.state.SearchUiState
+import com.example.ui.search.state.SearchResultState
+import com.example.ui.search.state.UserDetailState
 import com.example.ui.utils.FakeObjects.mockSearchQuery
 import com.example.ui.utils.FakeObjects.mockSearchResult
+import com.example.ui.utils.FakeObjects.mockUserDetail
+import com.example.ui.utils.FakeObjects.mockUserName
 import com.patronusgroup.presentation.common.MainDispatcherRule
 import io.kotest.common.runBlocking
 import io.kotest.matchers.shouldBe
@@ -21,16 +25,17 @@ internal class SearchViewModelTest {
     val mainDispatcherRule = MainDispatcherRule()
 
     private val getUsersUseCase: GetUsersUseCase = mock()
-    private val subject = SearchViewModel(getUsersUseCase)
+    private val getUserDetailsUseCase: GetUserDetailsUseCase = mock()
+    private val subject = SearchViewModel(getUsersUseCase, getUserDetailsUseCase)
 
     @Test
-    fun `loadDataIfNewSearchQuery should update result state if the use case returns success result`() {
+    fun `loadDataIfNewSearchQuery should update result state to success if the use case returns success result`() {
         runBlocking {
             whenever(getUsersUseCase.get(mockSearchQuery)) doReturn UserDataResult.Success(
                 mockSearchResult,
             )
             subject.loadDataIfNewSearchQuery(mockSearchQuery)
-            subject.searchUiState.value shouldBe SearchUiState.Success(
+            subject.searchResultState.value shouldBe SearchResultState.Success(
                 mockSearchQuery,
                 mockSearchResult.users,
             )
@@ -45,11 +50,33 @@ internal class SearchViewModelTest {
     }
 
     @Test
-    fun `loadDataIfNewSearchQuery should make the error state if the use case returns error`() {
+    fun `loadDataIfNewSearchQuery should update result state to error if the use case returns error`() {
         runBlocking {
             whenever(getUsersUseCase.get(mockSearchQuery)) doReturn UserDataResult.Error("")
             subject.loadDataIfNewSearchQuery(mockSearchQuery)
-            subject.searchUiState.value shouldBe SearchUiState.Error
+            subject.searchResultState.value shouldBe SearchResultState.Error
+        }
+    }
+
+    @Test
+    fun `loadDetails should update detail state to success if the use case returns success result`() {
+        runBlocking {
+            whenever(getUserDetailsUseCase.get(mockUserName)) doReturn UserDataResult.Success(
+                mockUserDetail,
+            )
+            subject.loadDetails(mockUserName)
+            subject.userDetailState.value shouldBe UserDetailState.Success(
+                mockUserDetail,
+            )
+        }
+    }
+
+    @Test
+    fun `loadDetails should update result state to error if the use case returns error`() {
+        runBlocking {
+            whenever(getUserDetailsUseCase.get(mockUserName)) doReturn UserDataResult.Error("")
+            subject.loadDetails(mockUserName)
+            subject.userDetailState.value shouldBe UserDetailState.Error
         }
     }
 }
