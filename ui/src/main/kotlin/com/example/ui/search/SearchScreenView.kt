531e8c2b37e82ad8a16cc15142fe587fd65b6711
Daniyal Idrees <>
2023-10-08 20:48:18 +0200

    User detail pane implementation and viewing logic added

--- ui/src/main/kotlin/com/example/ui/search/SearchScreenView.kt
+++ ui/src/main/kotlin/com/example/ui/search/SearchScreenView.kt
@@ -1,8 +1,10 @@
 package com.example.ui.search
 
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -14,58 +16,76 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.domain.model.User
-import com.example.ui.search.state.SearchUiState
+import com.example.domain.model.UserDetail
+import com.example.ui.detail.state.UserDetailState
+import com.example.ui.search.state.SearchResultState
 import com.example.ui.search.viewmodel.SearchViewModel
 import com.example.ui.views.DisplayImage
 import com.example.ui.views.LoadingView
 import com.example.ui.views.spaceL
 import com.example.ui.views.spaceS
+import com.example.ui.views.spaceXS
 
 @Composable
 fun SearchScreenView(
     viewModel: SearchViewModel,
-    navigateToDetail: (String) -> Unit,
 ) {
-    val viewState by viewModel.searchUiState.collectAsStateWithLifecycle()
+    val searchResultState by viewModel.searchResultState.collectAsStateWithLifecycle()
+    val detailDataState by viewModel.userDetailState.collectAsStateWithLifecycle()
 
     val verticalArrangement =
-        if (viewState !is SearchUiState.Success) Arrangement.Center else Arrangement.spacedBy(spaceS)
+        if (searchResultState is SearchResultState.Success) {
+            Arrangement.spacedBy(
+                spaceS,
+            )
+        } else {
+            Arrangement.Center
+        }
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(top = spaceS, start = spaceS, end = spaceS),
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = verticalArrangement,
     ) {
-        SearchView(onSearchButtonClick = viewModel::loadDataIfNewSearchQuery)
+        SearchBar(onSearchButtonClick = viewModel::loadDataIfNewSearchQuery)
 
-        when (viewState) {
-            is SearchUiState.Loading -> LoadingView()
-            is SearchUiState.Success -> UserListView(
-                users = (viewState as SearchUiState.Success).users,
-                navigateToDetail,
+        when (searchResultState) {
+            is SearchResultState.Loading -> LoadingView()
+            is SearchResultState.Success -> UserListView(
+                detailDataState,
+                users = (searchResultState as SearchResultState.Success).users,
+                onItemClick = viewModel::loadDetails,
             )
 
-            is SearchUiState.Error -> ShowErrorMessage()
+            is SearchResultState.Error -> ShowErrorMessage()
             else -> {}
         }
     }
@@ -81,11 +101,13 @@ private fun ShowErrorMessage() {
 }
 
 @Composable
-private fun SearchView(
+private fun SearchBar(
     onSearchButtonClick: (String) -> Unit,
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = spaceS, start = spaceS, end = spaceS),
         horizontalArrangement = Arrangement.spacedBy(
             space = spaceL,
             alignment = Alignment.CenterHorizontally,
@@ -114,6 +136,11 @@ private fun SearchInputField(onSearchButtonClick: (String) -> Unit) {
         label = { Text("Search a user") },
     )
 
+    SearchButton(onSearchClick)
+}
+
+@Composable
+private fun SearchButton(onSearchClick: () -> Unit) {
     Button(
         onClick = onSearchClick,
         shape = CircleShape,
@@ -128,20 +155,109 @@ private fun SearchInputField(onSearchButtonClick: (String) -> Unit) {
 }
 
 @Composable
-private fun UserListView(users: List<User>, onItemClick: (String) -> Unit) {
+private fun UserListView(
+    userDetailState: UserDetailState,
+    users: List<User>,
+    onItemClick: (String) -> Unit,
+) {
+    var shouldShowDetail by remember { mutableStateOf(false) }
+
     if (users.isEmpty()) {
         Text(
             text = "No results found",
         )
     } else {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(spaceS),
-        ) {
-            items(users) { user ->
-                UserListItem(
-                    user,
-                ) { onItemClick(user.name) }
+        Column {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(3f)
+                    .padding(top = spaceXS, start = spaceS, end = spaceS),
+                verticalArrangement = Arrangement.spacedBy(spaceS),
+            ) {
+                items(users) { user ->
+                    UserListItem(
+                        user,
+                    ) {
+                        shouldShowDetail = true
+                        onItemClick(user.name)
+                    }
+                }
             }
+
+            if (shouldShowDetail) {
+                when (userDetailState) {
+                    is UserDetailState.Success ->
+                        Box(modifier = Modifier.weight(1f)) {
+                            UserDetailView(userDetailState.detail) { shouldShowDetail = false }
+                        }
+
+                    is UserDetailState.Loading ->
+                        Box(modifier = Modifier.weight(1f)) {
+                            LoadingView()
+                        }
+
+                    is UserDetailState.Error -> ShowErrorMessage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun UserDetailView(detail: UserDetail, hideDetailAction: () -> Unit) {
+    Card(
+        modifier = Modifier.fillMaxSize(),
+        border = BorderStroke(1.dp, Color.Black),
+        shape = RectangleShape,
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent,
+        ),
+    ) {
+        TopEndCloseButton(onCloseButtonClick = hideDetailAction)
+        UserDetailContent(detail)
+    }
+}
+
+@Composable
+private fun TopEndCloseButton(onCloseButtonClick: () -> Unit) {
+    Box(modifier = Modifier.fillMaxWidth().padding(spaceXS)) {
+        OutlinedButton(
+            onClick = onCloseButtonClick,
+            modifier = Modifier.align(Alignment.TopEnd).size(20.dp),
+            shape = CircleShape,
+            contentPadding = PaddingValues(1.dp),
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "Close",
+            )
+        }
+    }
+}
+
+@Composable
+private fun UserDetailContent(detail: UserDetail) {
+    Column(
+        modifier = Modifier.padding(start = spaceS, end = spaceS),
+        verticalArrangement = Arrangement.spacedBy(spaceXS),
+    ) {
+        Text(
+            text = detail.userName,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+        )
+
+        if (detail.noOfFollowers != null) {
+            Text(
+                text = "Followers: ${detail.noOfFollowers}",
+            )
+        }
+
+        if (detail.noOfRepositories != null) {
+            Text(text = "Repositories: ${detail.noOfRepositories}")
         }
     }
 }
@@ -149,8 +265,7 @@ private fun UserListView(users: List<User>, onItemClick: (String) -> Unit) {
 @Composable
 private fun UserListItem(user: User, onItemClick: () -> Unit) {
     Column(
-        modifier = Modifier
-            .clickable(onClick = onItemClick),
+        modifier = Modifier.clickable(onClick = onItemClick),
         verticalArrangement = Arrangement.spacedBy(spaceS),
     ) {
         Row(
@@ -158,10 +273,22 @@ private fun UserListItem(user: User, onItemClick: () -> Unit) {
             horizontalArrangement = Arrangement.spacedBy(spaceS),
         ) {
             DisplayImage(user.imageUrl)
-            Text(
-                text = user.name,
-            )
+            Text(text = user.name)
         }
         Divider(color = Color.LightGray, thickness = 1.dp)
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun UserListPreview() {
+    UserListView(
+        userDetailState = UserDetailState.Loading,
+        users = listOf(
+            User(name = "user one", "fake url"),
+            User(name = "user two", "fake url"),
+            User(name = "user three", "fake url"),
+        ),
+        onItemClick = {},
+    )
 }
