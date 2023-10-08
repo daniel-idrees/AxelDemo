c68cdd686749c9b4ec2ea415f320af365066602b
Daniyal Idrees <>
2023-10-08 20:33:12 +0200

    user detail fetch implementation added in the view model

--- ui/src/main/kotlin/com/example/ui/search/viewmodel/SearchViewModel.kt
+++ ui/src/main/kotlin/com/example/ui/search/viewmodel/SearchViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.SearchResult
 import com.example.domain.model.UserDataResult
+import com.example.domain.model.UserDetail
+import com.example.domain.usecase.GetUserDetailsUseCase
 import com.example.domain.usecase.GetUsersUseCase
-import com.example.ui.search.state.SearchUiState
+import com.example.ui.detail.state.UserDetailState
+import com.example.ui.search.state.SearchResultState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,33 +18,59 @@ import javax.inject.Inject
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     private val getUsersUseCase: GetUsersUseCase,
+    private val getUserDetailsUseCase: GetUserDetailsUseCase,
 ) : ViewModel() {
 
-    private val _searchUiState =
-        MutableStateFlow<SearchUiState>(SearchUiState.Initial)
-    val searchUiState: StateFlow<SearchUiState>
-        get() = _searchUiState
+    private val _searchResultState =
+        MutableStateFlow<SearchResultState>(SearchResultState.Initial)
+    val searchResultState: StateFlow<SearchResultState>
+        get() = _searchResultState
+
+    private val _userDetailState =
+        MutableStateFlow<UserDetailState>(UserDetailState.Loading)
+    val userDetailState: StateFlow<UserDetailState>
+        get() = _userDetailState
 
     fun loadDataIfNewSearchQuery(searchQuery: String) {
-        if (searchQuery != (searchUiState.value as? SearchUiState.Success)?.searchQuery) {
-            loadData(searchQuery)
+        if (searchQuery != (searchResultState.value as? SearchResultState.Success)?.searchQuery) {
+            loadSearchResult(searchQuery)
         }
     }
 
-    private fun loadData(searchQuery: String) {
+    private fun loadSearchResult(searchQuery: String) {
         viewModelScope.launch {
-            _searchUiState.emit(SearchUiState.Loading)
+            _searchResultState.emit(SearchResultState.Loading)
             val result = getUsersUseCase.get(searchQuery)
-            updateUiState(searchQuery, result)
+            updateSearchResultState(searchQuery, result)
         }
     }
 
-    private suspend fun updateUiState(searchQuery: String, result: UserDataResult<SearchResult>) {
+    private suspend fun updateSearchResultState(
+        searchQuery: String,
+        result: UserDataResult<SearchResult>,
+    ) {
         val viewState = when (result) {
-            is UserDataResult.Success -> SearchUiState.Success(searchQuery, result.data.users)
-            is UserDataResult.Error -> SearchUiState.Error
+            is UserDataResult.Success -> SearchResultState.Success(searchQuery, result.data.users)
+            is UserDataResult.Error -> SearchResultState.Error
         }
 
-        _searchUiState.emit(viewState)
+        _searchResultState.emit(viewState)
+    }
+
+    fun loadDetails(userName: String) {
+        viewModelScope.launch {
+            _userDetailState.emit(UserDetailState.Loading)
+            val result = getUserDetailsUseCase.get(userName)
+            updateDetailState(result)
+        }
+    }
+
+    private suspend fun updateDetailState(result: UserDataResult<UserDetail>) {
+        val viewState = when (result) {
+            is UserDataResult.Success -> UserDetailState.Success(result.data)
+            is UserDataResult.Error -> UserDetailState.Error
+        }
+
+        _userDetailState.emit(viewState)
     }
 }
