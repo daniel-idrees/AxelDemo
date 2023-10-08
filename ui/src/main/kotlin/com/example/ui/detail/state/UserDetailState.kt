dfe4b2424c91db67058cbc14e8c533241e6164be
Daniyal Idrees <>
2023-10-08 20:58:42 +0200

    old file removed

--- ui/src/main/kotlin/com/example/ui/detail/state/UserDetailState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.ui.detail.state
-
-import com.example.domain.model.UserDetail
-
-sealed class UserDetailState {
-    data class Success(val detail: UserDetail) : UserDetailState()
-    object Error : UserDetailState()
-    object Loading : UserDetailState()
-}
