7a248d067a7ffef7bfc1395e87f1c3477f48eff6
Daniyal Idrees <>
2023-10-08 04:22:28 +0200

    application class added

--- app/src/main/kotlin/com/example/axeldemo/AxelApplication.kt
+++ app/src/main/kotlin/com/example/axeldemo/AxelApplication.kt
@@ -1,6 +1,7 @@
 package com.example.axeldemo
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AxelApplication: Application()
+class AxelApplication : Application()
